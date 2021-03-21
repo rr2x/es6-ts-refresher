df4a3c80eb3d53e6436016293d08f62f225a0cf3
rr2x <79620740+rr2x@users.noreply.github.com>
2021-03-22 07:43:17 +0800

    completed hooks-components project

--- Chap5/hooks-components/src/App.tsx
+++ Chap5/hooks-components/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState, useCallback, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Greeting } from './GreetingFunctional';
+import ListCreator, { ListItem } from './ListCreator';
 
 const App = () => {
 
@@ -26,18 +27,59 @@ const App = () => {
   }
 
   const [{ message, enteredName }, dispatch] = useReducer(reducer, initialState);
+  const [startCount, setStartCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch({ type: "enteredName", payload: e.target.value });
-    dispatch({ type: "message", payload: e.target.value });
+  const setCountCallback = useCallback(
+    () => {
+      const inc = count + 1 > startCount ? count + 1 : Number(count +1) + startCount;
+      setCount(inc);
+    },
+    [count, startCount],
+  );
+
+  const [listItems, setListItems] = useState<Array<ListItem>>();
+
+  useEffect(() => {
+    const li = [];
+    for(let i = 0; i < count; i++) {
+      li.push({id: i});
+    }
+    setListItems(li);
+  }, [count]);
+
+  const onWelcomeBtnClick = () => {
+    setCountCallback();
   }
+
+  const onChangeStartCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStartCount(Number(e.target.value));
+  }
+
+  console.log("App.tsx render");
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input value={enteredName} onChange={onChangeName}/>
-        <Greeting message={message} />
+        <Greeting
+          message={message}
+          enteredName={enteredName}
+          greetingDispatcher={dispatch} />
+
+        <div style={{ marginTop: '10px' }}>
+          <label>Enter a number and we'll increment it</label>
+          <br/>
+          <input
+            value={startCount} onChange={onChangeStartCount}
+            style={{width: '.75rem'}} />&nbsp;
+          <label>{count}</label>
+          <br/>
+          <button onClick={onWelcomeBtnClick}>Increment count</button>
+        </div>
+        <div>
+          <ListCreator listItems={listItems}/>
+        </div>
       </header>
     </div>
   );
--- Chap5/hooks-components/src/GreetingFunctional.tsx
+++ Chap5/hooks-components/src/GreetingFunctional.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
 interface GreetingProps {
-  message: string
+  enteredName: string;
+  message: string;
+  greetingDispatcher: React.Dispatch<{ type: string, payload: string }>;
 }
 
 export const Greeting = (props: GreetingProps) => {
   console.log("rendering greeting");
 
-  return (<div>{props.message}</div>);
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.greetingDispatcher({ type: "enteredName", payload: e.target.value });
+    props.greetingDispatcher({ type: "message", payload: e.target.value });
+  }
+
+  return (
+    <div>
+      <input value={props.enteredName} onChange={onChangeName} />
+      <div>
+        {props.message}
+      </div>
+    </div>
+  );
 }
