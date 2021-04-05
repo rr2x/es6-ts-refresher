6a506c589633c6b9e5a2112cf4f129000f8eb589
rr2x <79620740+rr2x@users.noreply.github.com>
2021-04-05 17:19:22 +0800

    start graphql

---
+++ Chap8/node-server/expressapp.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+
+const router = express.Router();
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`First middleware`);
+  next();
+});
+
+app.use(router);
+
+router.get(`/api/v1/users`, (req, res, next) => {
+  const users = [{ id: 1, username: "tom" }, { id: 2, username: "jon" }, { id: 3, username: "linda" }];
+  console.log(req.query.userid);;
+  const user = users.find(usr => usr.id == req.query.userid);
+  res.send(`User ${user?.username}`);
+});
+
+router.get(`api/v1/groups`, (req, res, next) => {
+  const groups = [{ id: 1, groupname: "Admins" }, { id: 2, groupname: "Users" }, { id: 3, groupname: "Employees" }];
+  const group = groups.find(grp => grp.id == req.body.groupid);
+  res.send(`Group ${group.groupname}`);
+});
+
+app.use((err, req, res, next) => res.status(500).send(err.message));
+
+app.listen({ port: 8000 }, () => console.log(`Express Node server has loaded!`));
